53b27a02be1fa40b46609fb96584997b91205958
Tokimitsu11 <icohuntpro@gmail.com>
2022-11-10 09:14:51 +0100

    .

---
+++ script.js
@@ -0,0 +1,47 @@
+// let posts=[{}, {}, {}]
+
+// let numbers = [0, 1, 2, 3, 4]
+// console.log(numbers)
+
+// numbers.push(5)
+// console.log(numbers)
+
+
+
+// let nums = [100, 500]
+
+// let number1 = +prompt("Enter number 1")
+// let number2 = +prompt("Enter number 2")
+// let number3 = +prompt("Enter number 3")
+
+// nums.push(number1, number2, number3)
+
+
+// console.log(nums)
+
+// let nums = [1, 2, 3, 4, 5]
+
+// console.log(nums[0])
+// console.log(nums[1])
+// console.log(nums[2])
+
+// for(let i = 0; i < nums.length; i++) {
+//     console.log(nums[i])
+// }
+// let nums = [10, 20, 30, 40, 50]
+// for(let i =0; i < nums.length; i++) {
+//     console.log(nums[i])
+
+// let nums = [32, 51, 60, 78, 105, 14, 10]
+
+// for(let i = 0; i < nums.length; i++) {
+//     if(nums[i] > 50) {
+//         console.log(nums[i])
+//     }
+// }
+
+
+// ДЗ.1. Есть массив [-10, 50, 60, 0, -100, 125]. Вывести в консоль только положительные+ числа.
+// ДЗ.2. Есть массив [11, 12, 15, 17, 18, 25, 26]. Вывести в консоль только четные числа. % - вычислить остаток от деления. 10 % 2 = 0. 11 % 2 = 1
+// ДЗ.3  Есть массив [10, 20, 30 ,40 , 50, 60]. Найти сумму чисел в массиве и вывести их в консоль.let sum =  sum + arr[i]
+// ДЗ.4. Есть массив [-10, 50, 60, 0, -100, 125]. Найти произведение отрицательных чисел
